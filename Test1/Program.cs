a72f5874a717e64fae0f52b14dca529ad466efb2
Evgeniya <jkj_z@mail.ru>
2023-10-30 17:55:08 +0300

    Add Test 1

---
+++ Test1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int NaturalDegree (int A, int B)
+{
+    int result = A;
+    for(int i = 1; i < B; i++)
+    {
+       result *= A;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы ввели: {number}. Введите степень для возведения");
+int natural = Convert.ToInt32(Console.ReadLine());
+int naturalDegree = NaturalDegree(number, natural);
+Console.WriteLine($"Число {number} в степени {natural} равно {naturalDegree}");
