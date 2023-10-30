0f7aa98238e12cd102d4d05e74d02c314ad868a6
Evgeniya <jkj_z@mail.ru>
2023-10-30 18:19:47 +0300

    Add Test 2

---
+++ Test2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SummOfDigits (string A)
+{
+    int len = A.Length;
+    int result = 0;
+    for(int i = 0; i < len; i++)
+    {
+        int digit = int.Parse(A[i].ToString());
+        result = result + digit;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int summOfDigits = SummOfDigits(number);
+Console.WriteLine($"Сумма цифр числа '{number}' равна '{summOfDigits}'");
